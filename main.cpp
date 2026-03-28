8091e9e1ee305ec44edce545be04d2a4f7c81b2a
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-03-28 17:25:02 +0000

    implement recursion and count file sizes into bins

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <dirent.h>
+#include <sys/stat.h>
 using namespace std;
+
+const int MAX_BINS = 1000;
+int histogram[MAX_BINS] = {0};
+int binSize;
 
 void traverseFolder(string dirPath) {
     DIR* directory = opendir(dirPath.c_str());
@@ -13,8 +18,16 @@ void traverseFolder(string dirPath) {
 
         string fullPath = dirPath + "/" + entryName;
 
-        // For now, just print directory paths
-        cout << "Found: " << fullPath << endl;
+        struct stat fileInfo;
+        if (stat(fullPath.c_str(), &fileInfo) == -1) continue;
+
+        if (S_ISDIR(fileInfo.st_mode)) {
+            traverseFolder(fullPath); // recursive call
+        } else if (S_ISREG(fileInfo.st_mode)) {
+            long fSize = fileInfo.st_size;
+            int binIdx = fSize / binSize;
+            if (binIdx < MAX_BINS) histogram[binIdx]++;
+        }
     }
 
     closedir(directory);
@@ -24,6 +37,9 @@ int main() {
     string folderInput;
     cout << "Enter directory path: ";
     cin >> folderInput;
+
+    cout << "Enter bin size: ";
+    cin >> binSize;
 
     traverseFolder(folderInput);
     return 0;
